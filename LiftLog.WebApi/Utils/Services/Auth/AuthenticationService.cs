fb3ae0a74311910053aa30d82731ef5b05cc4052
Mahammad Manafov <manafov0817@gmail.com>
2024-04-09 22:44:12 +0400

    Logger problem is fixed

--- LiftLog.WebApi/Utils/Services/Auth/AuthenticationService.cs
+++ LiftLog.WebApi/Utils/Services/Auth/AuthenticationService.cs
@@ -1,6 +1,7 @@
-﻿using LiftLog.WebApi.Utils.Models.Identity;
-using LiftLog.WebApi.Utils.Services.Emailing;
+﻿using AutoMapper;
+using LiftLog.WebApi.Utils.Models.Identity;
 using Microsoft.AspNetCore.Identity;
+using NLog;
 
 namespace LiftLog.WebApi.Utils.Services.Auth
 {
@@ -9,48 +10,60 @@ namespace LiftLog.WebApi.Utils.Services.Auth
         private readonly JwtTokenService _jwtTokenService;
         private UserManager<User> _userManager;
         private SignInManager<User> _signinManager;
+        private readonly IMapper _mapper;
+        private readonly Logger Logger = LogManager.GetLogger("AuthLogger");
 
         public AuthenticationService(UserManager<User> userManager,
                                      JwtTokenService jwtTokenService,
                                      SignInManager<User> signinManager,
-                                     IHostEnvironment hostEnvironment,
-                                     IEmailSender emailSender)
+                                     IMapper mapper,
+                                     ILogger<AuthenticationService> logger)
         {
             _jwtTokenService = jwtTokenService;
             _userManager = userManager;
             _signinManager = signinManager;
-            _hostEnvironment = hostEnvironment;
-            _emailSender = emailSender;
+            _mapper = mapper;
         }
 
         public async Task<string?> AuthenticateUser(string userEmail, string password)
         {
-            // Authentication logic here...
-
-            var user = await _userManager.FindByNameAsync(userEmail);
-
-            if (user == null)
-                user = await _userManager.FindByEmailAsync(userEmail);
+            Logger.Info($"Searching user by email: {userEmail}");
+            var user = await _userManager.FindByEmailAsync(userEmail);
 
             if (user == null)
+            {
+                Logger.Info($"Searching user by username: {userEmail}");
+                user = await _userManager.FindByNameAsync(userEmail);
+            }
+
+            if (user == null)
+            {
+                Logger.Info($"Username or email not found: {userEmail}");
                 return null;
-
-            //if (!await _userManager.IsEmailConfirmedAsync(user))
-            //{
-            //    return null;
-            //}
+            }
 
             var result = await _signinManager.PasswordSignInAsync(user, password, true, false);
 
-            return result.Succeeded ? _jwtTokenService.GenerateToken(user.UserName, user.Email) : null;
+            if (!result.Succeeded)
+            {
+                Logger.Info($"Invalid password for {userEmail}: {password.Substring(0, 3)}");
+                return null;
+            }
+
+            string resToken = result.Succeeded ? _jwtTokenService.GenerateToken(user.UserName, user.Email) : null;
+
+            Logger.Info($"Signing In User. Username: {user.UserName}, First 10 digits of token: {resToken.Substring(0, 10)}");
+
+            return resToken;
         }
         public async Task<(IdentityResult, User)> CreateUserAsync(RegisterRequestModel model)
         {
             var result = new IdentityResult();
-            // Automap here
-            var user = new User { FirstName = model.FirstName, LastName = model.LastName, UserName = model.UserName, Email = model.Email };
+
+            var user = _mapper.Map<User>(model);
             try
             {
+                Logger.Info($"Creating User: {model.ToString()}");
                 result = await _userManager.CreateAsync(user, model.Password);
             }
             catch { }
